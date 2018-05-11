9efb97d26ee380ace37140bd99e8d708ebebbf44
Atmyre <gaintseva@phystech.edu>
2018-05-12 01:20:16 +0300

    many markers

--- src/using_markers/src/basic_shapes.cpp
+++ src/using_markers/src/basic_shapes.cpp
@@ -1,53 +1,58 @@
 #include <ros/ros.h>
 #include <visualization_msgs/Marker.h>
 #include <car_detect/TrackedObject.h>
+#include <car_detect/TrackedObjects.h>
 
 
 ros::Publisher marker_pub;
 uint32_t shape = visualization_msgs::Marker::CUBE;
 
-void bboxCallback(const car_detect::TrackedObjectConstPtr tracked_object)
+void bboxCallback(const car_detect::TrackedObjectsConstPtr tracked_objects)
    {
 
-      car_detect::TrackedObject object = *tracked_object;
-      
-      visualization_msgs::Marker marker;
-      marker.header.frame_id = "/car_frame";
-      marker.header.stamp = ros::Time::now();
-      marker.ns = "basic_shapes";
-      marker.id = 0;
-      marker.type = shape;
-      marker.action = visualization_msgs::Marker::ADD;
+      car_detect::TrackedObjects objects = *tracked_object;
 
-      marker.pose.position.x = object.pose.pose.position.x;
-      marker.pose.position.y = object.pose.pose.position.y;
-      marker.pose.position.z = object.pose.pose.position.z;
-      // marker.pose.position.x = 0;
-      // marker.pose.position.y = 0;
-      // marker.pose.position.z = 0;
-      marker.pose.orientation.x = 0.0;
-      marker.pose.orientation.y = 0.0;
-      marker.pose.orientation.z = 0.0;
-      marker.pose.orientation.w = 0.0;
 
-      // Set the scale of the marker -- 1x1x1 here means 1m on a side
-      marker.scale.x = object.dims.dimensions.x;
-      marker.scale.y = object.dims.dimensions.y;
-      marker.scale.z = object.dims.dimensions.z;
-      // marker.scale.x = 1.0;
-      // marker.scale.y = 1.0;
-      // marker.scale.z = 1.0;
+      for (int i = 0; i < objects.trackedObjects.size(); ++i) {
+        car_detect::TrackedObject object = objects.trackedObjects[i];
+        visualization_msgs::Marker marker;
+        marker.header.frame_id = "/car_frame";
+        marker.header.stamp = ros::Time::now();
+        marker.ns = "basic_shapes" + i;
+        marker.id = i;
+        marker.type = shape;
+        marker.action = visualization_msgs::Marker::ADD;
 
-      // Set the color -- be sure to set alpha to something non-zero!
-      marker.color.r = 1.0f;
-      marker.color.g = 0.5f;
-      marker.color.b = 0.5f;
-      marker.color.a = 1.0;
+        marker.pose.position.x = object.pose.pose.position.x;
+        marker.pose.position.y = object.pose.pose.position.y;
+        marker.pose.position.z = object.pose.pose.position.z;
+        // marker.pose.position.x = 0;
+        // marker.pose.position.y = 0;
+        // marker.pose.position.z = 0;
+        marker.pose.orientation.x = 0.0;
+        marker.pose.orientation.y = 0.0;
+        marker.pose.orientation.z = 0.0;
+        marker.pose.orientation.w = 0.0;
 
-      marker.lifetime = ros::Duration();
+        // Set the scale of the marker -- 1x1x1 here means 1m on a side
+        marker.scale.x = object.dims.dimensions.x;
+        marker.scale.y = object.dims.dimensions.y;
+        marker.scale.z = object.dims.dimensions.z;
+        // marker.scale.x = 1.0;
+        // marker.scale.y = 1.0;
+        // marker.scale.z = 1.0;
 
-      // Publish the data.
-       marker_pub.publish (marker);
+        // Set the color -- be sure to set alpha to something non-zero!
+        marker.color.r = 1.0f;
+        marker.color.g = 0.5f;
+        marker.color.b = 0.5f;
+        marker.color.a = 1.0;
+
+        marker.lifetime = ros::Duration();
+
+        // Publish the data.
+         marker_pub.publish (marker);
+      }
    }
 
 
